44a3a54e292e24b40b226695be865e59e4a56eb6
Ioan Latrovaliev <latrovalievioan@gmail.com>
2022-12-02 16:22:59 +0200

    solve day 2 part 2

--- day2.hs
+++ day2.hs
@@ -14,11 +14,24 @@ getScore line = case line of
   "C Y" -> 2
   "C Z" -> 6
 
+getScore2 :: String -> Int
+getScore2 line = case line of
+  "A X" -> 3
+  "A Y" -> 4
+  "A Z" -> 8
+  "B X" -> 1
+  "B Y" -> 5
+  "B Z" -> 9
+  "C X" -> 2
+  "C Y" -> 6
+  "C Z" -> 7
+
 main :: IO ()
 main = do
   s <- splitOn "\n" <$> readFile "input_day2.txt"
   let input = init s
 
   let gg = sum $ map getScore input
+  let gg2 = sum $ map getScore2 input
 
-  print gg
+  print (gg, gg2)
