ab9c421329081ed3c3747f9101d65bbbc0535ffa
Ioan Latrovaliev <latrovalievioan@gmail.com>
2022-12-03 16:47:13 +0200

    solve day3 part2

--- day3/day3.hs
+++ day3/day3.hs
@@ -5,12 +5,19 @@ import Data.Char (ord)
 
 main :: IO ()
 main = do
-  s <- splitOn "\n" <$> readFile "input_day3"
+  s <- splitOn "\n" <$> readFile "input1"
   let input = init s
 
-  let result = foldl' step 0 input
+  let part1 = foldl' step 0 input
+  print part1
   
-  print result
+  let chunked = chunksOf 3 input
+
+  let commons = map head (map nub (map findCommon3 chunked))
+
+  let part2 = foldl' (\acc curr -> acc + priority curr) 0 commons
+
+  print part2
   
   where
     step :: Int -> String -> Int
@@ -19,6 +26,8 @@ main = do
     findCommon :: (String, String) -> Char
     findCommon (xs, ys) = head $ filter (\x -> x `elem` ys) xs
 
+    findCommon3 [a, b, c] = filter(\x -> (x `elem` b) && (x `elem` c)) a
+
     priority :: Char -> Int
     priority x
       | ord x >= 97 = ord x - 96
@@ -26,4 +35,3 @@ main = do
 
     halve :: String -> (String, String)
     halve word = splitAt (length word `div` 2) word
-
